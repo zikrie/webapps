b2e7be40393061abd29fb79e6f135a0b0a2507bd
atikahiskandar <43739599+atikahiskandar@users.noreply.github.com>
2019-09-13 15:57:58 +0800

    Language

--- resources/views/scheme/noticeAccident/Revision/ob_profile/PK/ob_info.blade.php
+++ resources/views/scheme/noticeAccident/Revision/ob_profile/PK/ob_info.blade.php
@@ -141,12 +141,12 @@
                         <!--button type="button" class="btn btn waves-effect waves-light btn-secondary">@lang('insuredPerson.cancel')</button>
                                 <button type="button" class="btn btn waves-effect waves-light btn-secondary">@lang('insuredPerson.clear')</button-->
                         <button type="submit"
-                            class="btn btn waves-effect waves-light btn-success">@lang('insuredPerson.save')</button>
+                            class="btn btn waves-effect waves-light btn-success">@lang('button.save')</button>
                         <button type="button" class="btn btn waves-effect waves-light btn-info"
-                            onclick="submitform()">@lang('noticetype.reset')</button>
+                            onclick="submitform()">@lang('button.reset')</button>
                         <button type="button" class="btn waves-effect waves-light btn-secondary" id='btncancelacc'
-                            onclick="window.location='/noticetype'">@lang('noticetype.cancel')</button>
-                            <button type="button" class="btn waves-effect waves-light btn-secondary" id='btncancelacc' onclick="window.location='/obform_od'">@lang('noticetype.back')</button>
+                            onclick="window.location='/noticetype'">@lang('button.cancel')</button>
+                            <button type="button" class="btn waves-effect waves-light btn-secondary" id='btncancelacc' onclick="window.location='/obform_od'">@lang('button.back')</button>
                     </div>
                 </form>
             </div>
