d7b25c84911af100186af18a92a961dbc433267e
Ahmad Alif <alifsafiq@gmail.com>
2019-09-26 16:02:32 +0800

    scheme, notice death, new claim, sco

---
+++ resources/views/scheme/noticeDeath/newClaim/SAO/inspector_info.blade.php
@@ -0,0 +1,25 @@
+<div class="row">
+   <div class="col-12">
+       <div class="card">
+           <div class="card-body">
+               <div class="p-20">
+                   <form action="#">
+                   <div class="row p-t-20">
+                       <div class="col-md-4">
+                           <div class="form-group">
+                               <label class="control-label">Inspector Name</label>
+                               <input type="text" name="sender_note" id="sender_note" class="form-control">
+                           </div>
+                       </div>
+                   </div>
+                   
+                   <div class="form-actions">
+                       <button type="submit" class="btn btn waves-effect waves-light btn-success">@lang('button.save')</button>
+                       <button type="button" class="btn btn waves-effect waves-light btn-info" onclick="submitform()">@lang('button.reset')</button>
+                   </div>            
+                   </form>
+               </div>
+           </div>
+       </div>
+   </div>
+</div>
